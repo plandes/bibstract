ac90e6d9bcd804e810037c4e9d57e178fc20e763
Paul Landes <landes@mailc.net>
2021-03-04 09:44:23 -0600

    fix config

--- src/python/zensols/bibstract/config.py
+++ src/python/zensols/bibstract/config.py
@@ -3,15 +3,12 @@
 """
 __author__ = 'Paul Landes'
 
-from zensols.config import (
-    CommandLineConfig,
-    ExtendedInterpolationConfig,
-)
+from zensols.config import CommandLineConfig, ExtendedInterpolationConfig
 
 
 class AppConfig(ExtendedInterpolationConfig, CommandLineConfig):
     def __init__(self, *args, **kwargs):
-        super(AppConfig, self).__init__(*args, default_expect=True, **kwargs)
+        super(AppConfig, self).__init__(*args, **kwargs)
 
     def set_defaults(self, master_bib: str = None):
         self.set_default('master_bib', None, master_bib)
